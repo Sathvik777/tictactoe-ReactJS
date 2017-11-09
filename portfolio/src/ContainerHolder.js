597687ce6601da36ad2af4e59344bf55ca0ab7f5
Sathvik Katam <katam.sathvik@gmail.com>
2017-11-09 14:11:54 +0100

    started implenting 1st container

---
+++ portfolio/src/ContainerHolder.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './ContainerHolder.css';
+class ContainerHolder  extends Component {
+  render() {
+    return (
+      <div className="fill-backround">
+        <div className ="picture-holder animated bounce">
+          <img  className="image" src={"/assets/sathvik.jpg"} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ContainerHolder;
+
+
