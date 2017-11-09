16380f3130cee144ec2f453132f858db7f02e095
Sathvik Katam <katam.sathvik@gmail.com>
2017-11-09 14:11:28 +0100

    completed nav bar

--- portfolio/src/NavBar.js
+++ portfolio/src/NavBar.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './NavBar.css';
 
 class NavBar extends Component {
   render() {
     return (
       <div >
-      <nav className="navbar navbar-light bg-faded">
+      <nav className="navbar navbar-dark bg-faded">
       <ul className="main-nav">
-      <li><a>Home</a></li>
-      <li><a href="#">Apps</a></li>
-      <li><a >About</a></li>
-      <li><a >Contact</a></li>
+      <li><a href="#">Home</a></li>
+      <li><a>Apps</a></li>
+      <li><a>About</a></li>
+      <li><a>Contact</a></li>
      </ul>
      </nav>
     </div>
